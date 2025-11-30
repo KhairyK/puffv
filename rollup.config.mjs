2e31eb636a3610f8c2b963576dbf3c0226d6d110
Sholehuddin Khairy <musickhairy@gmail.com>
2025-11-30 13:03:56 +0700

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,25 +1,52 @@
 import resolve from "@rollup/plugin-node-resolve";
-import { terser } from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser";
 
-export default {
-  input: "dist/puffv.min.js",
-  output: [
-    {
+const banner = `
+/*
+  2025 © OpenDN Foundation (PHPin)
+  PuffvJS — v0.0.1
+  MIT LICENSE (https://kyrtproduct.license.kyrt.my.id)
+  Support UMD, AMD, ESM, RequireJS
+*/
+`;
+
+export default [
+  {
+    input: "dist/puffv.min.js",
+    output: {
       file: "dist/puffv.umd.js",
       format: "umd",
       name: "Puffv",
+      banner
     },
-    {
+    plugins: [resolve()]
+  },
+  {
+    input: "src/puffv.js",
+    output: {
+      file: "dist/puffv.umd.min.js",
+      format: "umd",
+      name: "Puffv",
+      banner
+    },
+    plugins: [resolve(), terser()]
+  },
+  {
+    input: "src/puffv.js",
+    output: {
       file: "dist/puffv.esm.js",
       format: "es",
+      banner
     },
-    {
+    plugins: [resolve(), terser()]
+  },
+  {
+    input: "src/puffv.js",
+    output: {
       file: "dist/puffv.cjs",
       format: "cjs",
-    }
-  ],
-  plugins: [
-    resolve(),
-    terser()
-  ]
-};
+      banner
+    },
+    plugins: [resolve(), terser()]
+  }
+];
